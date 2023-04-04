a92d554924b9494638e65bb4aa3e034ecf9ec479
Jyothsna-Siripuram <s92jyothsna@gmail.com>
2023-04-04 13:18:34 +0530

    add stylesheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'docs/index'
-  get 'docs/create'
-  get 'docs/edit'
-  get 'docs/new'
-  get 'docs/update'
-  get 'docs/destroy'
   get 'welcome/index'
+
   root 'welcome#index'
+  authenticated :user do 
+    root 'docs#index', as: "authenticated_root"
+  end
   resources :docs
 end
