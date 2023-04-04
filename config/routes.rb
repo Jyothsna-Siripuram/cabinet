c0edd0fe99b362b7179e7f2af324c97d8b7b4557
Jyothsna-Siripuram <s92jyothsna@gmail.com>
2023-04-04 10:52:24 +0530

    add doc controller and methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'docs/index'
+  get 'docs/create'
+  get 'docs/edit'
+  get 'docs/new'
+  get 'docs/update'
+  get 'docs/destroy'
   get 'welcome/index'
   root 'welcome#index'
+  resources :docs
 end
